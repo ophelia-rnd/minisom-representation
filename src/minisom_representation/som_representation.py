cf442387704108ba0858044a3cfbcff029ab0ab9
Matthew Balogh <meld-03-spouts@icloud.com>
2026-08-17 01:24:44 +0200

    Store activation map in fitted representation; related refinements

--- src/minisom_representation/som_representation.py
+++ src/minisom_representation/som_representation.py
@@ -1,11 +1,13 @@
 from minisom import MiniSom
 from minisom_representation.som_hyperparameter import calc_som_hyparams
+from typing import Literal
 
 class SomRepresentation():
 
     def __init__(self, d1, d2, sigma=1.0, topology="rectangular", learning_rate=0.5, num_iteration=20,
                     decay_function="asymptotic_decay", sigma_decay_function="asymptotic_decay",
                     neighborhood_function='gaussian', activation_distance='euclidean',
+                    distance_map_scaling:Literal["mean", "sum"]="mean",
                     random_seed=None, verbose=False):
         self.d1 = d1
         self.d2 = d2
@@ -17,6 +19,7 @@ class SomRepresentation():
         self.sigma_decay_function = sigma_decay_function
         self.neighborhood_function = neighborhood_function
         self.activation_distance = activation_distance
+        self.distance_map_scaling = distance_map_scaling
         self.random_seed = random_seed
         self.verbose = verbose
 
@@ -28,14 +31,27 @@ class SomRepresentation():
 
     @property
     def som(self):
+        self.__require_fitted()
         return self.som_
 
     @property
+    def distance_map(self):
+        self.__require_fitted()
+        return self.distance_map_
+
+    @property
+    def activation_map(self):
+        self.__require_fitted()
+        return self.activation_map_.astype(int)
+
+    @property
     def quantization_error(self):
+        self.__require_fitted()
         return self.QE_
 
     @property
     def topographic_error(self):
+        self.__require_fitted()
         return self.TE_
 
     def fit(self, X):
@@ -67,8 +83,13 @@ class SomRepresentation():
         TE = som.topographic_error(X)
 
         self.som_ = som
+        self.distance_map_ = som.distance_map(scaling=self.distance_map_scaling)
+        self.activation_map_ = som.activation_response(X)
+
         self.QE_ = QE
         self.TE_ = TE
+
+        self.fitted_ = True
 
         if self.verbose:
             print("\n", "An SOM representation has been fitted as follows:")
@@ -84,3 +105,6 @@ class SomRepresentation():
             print(f"Topographic Error (TE): \t{TE}")
 
         return self
+
+    def __require_fitted(self):
+        assert hasattr(self, "fitted_") and self.fitted_, "This SOM representation has not been fitted yet."
